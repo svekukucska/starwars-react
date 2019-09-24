5f7c84727b4e57432c6714f180ccd0223cc8ee13
Eduard Kukucska <eduard.kukucska@softvision.ro>
2019-09-24 13:50:04 +0300

    [SW-3] Set-up React Router

---
+++ src/pages/Home/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = (props) =>  <h1>Homepage - Welcome!</h1>;
+
+export default Home;
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = (props) => <h1>NotFound - Error 404!!</h1>;
+
+export default NotFound;
