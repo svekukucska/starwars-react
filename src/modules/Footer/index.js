9065cd0719f75548917862efe3859066a87b7c18
Ionut Zsollei <ionut.zsollei@softvision.ro>
2019-09-25 15:51:36 +0300

    [SW-4] Sticky Footer

---
+++ src/modules/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css'
+
+const Footer = () => (
+  <div className="footer" sticky="bottom">
+    <span>&copy; Copyright 2019</span>
+  </div>
+);
+
+export default Footer;
