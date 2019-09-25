ecc74dd8992e0c8e560cc18a0ffce4ba9001389c
Eduard Kukucska <eduard.kukucska@softvision.ro>
2019-09-25 10:44:19 +0300

    [SW-8] Add a fixed main menu

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Footer from './modules/Footer';
+import MainMenu from './modules/MainMenu';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <MainMenu />
           <div className="page-container">
             <Switch>
               <Route path="/" exact component={Home} />
+              <Route path="/home" exact component={Home} />
               <Route component={NotFound} />
             </Switch>
           </div>
           <Footer />
         </div>
-      </Router>
+      </Router >
     );
   }
 }
