9ad37b778310e788c681798e660820e6a4246b29
Eduard Kukucska <eduard.kukucska@softvision.ro>
2019-10-02 10:42:53 +0300

    [SW-11] Make the main menu collapse

--- src/components/MainMenu/index.js
+++ src/components/MainMenu/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import '../../App.css'; 
+import '../../App.css';
 import { NavLink } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import navLogo from '../../assets/nav_logo.svg';
 import './index.css';
 
 const MainMenu = (props) => (
-          <Navbar bg="dark" variant="dark" fixed="top">
-                <Navbar.Brand as={NavLink} to="/">
-                  <img className="nav-logo" src={navLogo} alt="Star Wars nav logo" />
-                </Navbar.Brand>
-                <Nav className="m-auto">
-                  <Nav.Link as={NavLink} to="/movies">Star Wars Movies</Nav.Link>
-                  <Nav.Link as={NavLink} to="/planets">Planets</Nav.Link>
-                </Nav>
-          </Navbar>
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+    <Navbar.Brand as={NavLink} to="/">
+      <img className="nav-logo" src={navLogo} alt="Star Wars nav logo" />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="m-auto">
+        <Nav.Link as={NavLink} to="/movies">Star Wars Movies</Nav.Link>
+        <Nav.Link as={NavLink} to="/planets">Planets</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default MainMenu;
