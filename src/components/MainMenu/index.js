da200246e87edc5bde1bafa00c79aa5c27d394e3
svekukucska <54104778+svekukucska@users.noreply.github.com>
2019-10-15 10:20:02 +0300

    [SW-26] Menu is overlapping (#16)

--- src/components/MainMenu/index.js
+++ src/components/MainMenu/index.js
@@ -8,13 +8,14 @@ class MainMenu extends React.Component {
   state = {
     movies: null,
     dropdownOpen: false,
+    expanded: false,
   };
 
   componentDidMount() {
-      getMovies().then(({results}) => {
+    getMovies().then(({ results }) => {
       const movies = results.sort((a, b) => (a.episode_id > b.episode_id) ? 1 : ((b.episode_id > a.episode_id) ? -1 : 0));
-      this.setState({movies});
-      });
+      this.setState({ movies });
+    });
   };
 
   handleDropdownOpen = () => {
@@ -29,27 +30,43 @@ class MainMenu extends React.Component {
     });
   }
 
+  closeMainMenu = () => {
+    this.setState({ expanded: false });
+  }
+
+  goToMovieList = () => {
+    this.props.history.push('/movies');
+    this.closeMainMenu();
+  }
+
   render() {
     const {
       movies,
       dropdownOpen,
+      expanded,
     } = this.state;
-    const linkMovieList = (<span onClick={() => this.props.history.push('/movies')}> Star Wars Movies</span>);
+    const linkMovieList = (<span onClick={this.goToMovieList}> Star Wars Movies</span>);
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">        
-      <Container>
-        <Navbar.Brand as={NavLink} to="/">
-          <img className="nav-logo" src={navLogo} alt="Star Wars nav logo" />
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top"
+        onToggle={() => this.setState({ expanded: !expanded })}
+        expanded={expanded}
+      >
+        <Container>
+          <Navbar.Brand as={NavLink} to="/">
+            <img className="nav-logo" src={navLogo} alt="Star Wars nav logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="m-auto">
-              <NavDropdown variant="tabs" title={linkMovieList} onMouseEnter={this.handleDropdownOpen} onMouseLeave={this.handleDropdownClose} show={dropdownOpen}>
+              <NavDropdown variant="tabs" title={linkMovieList} onMouseEnter={this.handleDropdownOpen} onMouseLeave={this.handleDropdownClose} 
+              show={dropdownOpen}>
                 {movies && movies.map(({ episode_id, title }) => (
-                  <NavDropdown.Item as={NavLink} to={`/movies/${episode_id}`} key={episode_id}>{title}</NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to={`/movies/${episode_id}`} key={episode_id} onClick={this.closeMainMenu}>
+                    {title}
+                  </NavDropdown.Item>
                 ))}
               </NavDropdown>
-              <Nav.Link as={NavLink} to="/planets">Planets</Nav.Link>
+              <Nav.Link as={NavLink} to="/planets" onClick={this.closeMainMenu} >Planets</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
