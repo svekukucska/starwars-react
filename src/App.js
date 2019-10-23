7bb1d0b30175c450eafc6b58efa7f850c7bc7fb8
svekukucska <54104778+svekukucska@users.noreply.github.com>
2019-10-23 16:32:35 +0300

    [sw-15] Add page name class to page-container element (#19)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
 import Footer from './components/Footer';
 import MainMenu from './components/MainMenu';
 import { Container } from 'react-bootstrap';
-import StarWarsMovies from './pages/StarWarsMovies';
-import Movie from './pages/Movie/index';
+import { Pages } from './pages/Routes';
 
 class App extends Component {
   render() {
@@ -14,16 +11,20 @@ class App extends Component {
       <Router>
         <div className="App">
           <MainMenu />
-          <div className="page-container">
-            <Container>
-              <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/movies" exact component={StarWarsMovies} />
-                <Route path="/movies/:episodeId" exact component={Movie} />
-                <Route component={NotFound} />
-              </Switch>
-            </Container>
-          </div>
+          <Switch >
+            {Pages.map(({ className, component: PageComponent, props }) => (
+              <Route {...props} key={`page-${className}`} render={(routeProps) => {
+                return (
+                  <div className={`page-container page-${className}`}>
+                    <Container>
+                      <PageComponent {...routeProps} />
+                    </Container>
+                  </div>
+                );
+              }} />
+            )
+            )}
+          </Switch>
           <Footer />
         </div>
       </Router >
