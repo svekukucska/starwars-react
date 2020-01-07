862e38c95eaec35c8beed820d3c89b5780e589f8
svekukucska <54104778+svekukucska@users.noreply.github.com>
2020-01-07 14:51:14 +0200

    [SW-19] Refactor Main Menu Dropdown to use Redux (#29)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import MovieList from './MovieList';
 
 const rootReducers = combineReducers({
-
+  MovieListReducer: MovieList,
 });
 
 export default rootReducers;
